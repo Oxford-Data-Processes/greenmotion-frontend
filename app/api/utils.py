0b73e0d721a9bd8ab98c75ee9b1d1d9a1b733ff4
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-11-06 14:49:53 +0000

    Update api reference

--- app/api/utils.py
+++ app/api/utils.py
@@ -1,6 +1,6 @@
 import requests
 
-BASE_URL = "https://84ggcq4ff2.execute-api.eu-west-2.amazonaws.com/dev/"
+BASE_URL = "https://zhjlsusdz3.execute-api.eu-west-2.amazonaws.com/dev/"
 
 # BASE_URL = "http://localhost:8000/"
 
